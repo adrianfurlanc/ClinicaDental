e2674b926de73691379ff9c51727410d5bd840fb
Adrian Furlan <adrian.furlan.c@gmail.com>
2021-06-01 13:49:52 +0200

    Add all required folders

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./config/mongoose.js');
+const app = express();
+const port = 3000;
+// const router = require('./router');
+
+// Middleware
+app.use(express.json());
+// app.use(router);
+
+
+//Connecting to the database
+db.then(()=>{
+//Starting server
+    app.listen(port, ()=> console.log(`Servidor levantado en http://localhost:${port}`));
+})
+.catch((err)=> console.log(err.message));
